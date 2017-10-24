e4c4555f39eb9c1f709e736535c6b71d105e454f
TennnyZhuang <zty0826@gmail.com>
2017-10-24 19:41:28 +0800

    2017-10-24 19:41:28

--- bundle.js
+++ bundle.js
@@ -74,6 +74,18 @@
     canvas.width = width;
     canvas.height = height;
     document.body.appendChild(canvas);
+    var keylabel = document.createElement('div');
+    keylabel.style.width = '200px';
+    keylabel.style.height = '100px';
+    keylabel.style.lineHeight = '100px';
+    keylabel.style.position = 'fixed';
+    keylabel.style.top = '20px';
+    keylabel.style.left = '20px';
+    keylabel.style.backgroundColor = '#ccc';
+    keylabel.style.fontSize = '2em';
+    keylabel.style.textAlign = 'center';
+    keylabel.innerText = '';
+    document.body.appendChild(keylabel);
     var ctx = canvas.getContext('2d');
     ctx.fillStyle = 'black';
     var points = [];
@@ -238,8 +250,7 @@
                 ctx.stroke();
             }
         };
-        Polygon.prototype.fill = function (color) {
-            if (color === void 0) { color = new Color(0, 0, 0); }
+        Polygon.prototype.fill = function () {
             if (this.state !== PolygonState.POLYGON_DONE) {
                 return;
             }
@@ -470,6 +481,7 @@
         };
         return Color;
     }());
+    var color = new Color(0, 0, 0);
     function drawPixel(x, y) {
         ctx.fillRect(x, y, 1, 1);
     }
@@ -514,6 +526,7 @@
         repaint();
     });
     canvas.addEventListener('contextmenu', function (event) {
+        keylabel.innerText = 'Right Click';
         console.log(poly);
         if (poly.state === PolygonState.POLYGON_OUTER) {
             poly.state = PolygonState.POLYGON_INNER;
@@ -541,10 +554,12 @@
         repaint();
     });
     canvas.addEventListener('click', function (event) {
+        keylabel.innerText = 'Left Click';
         points.push(new Point(event.pageX, event.pageY));
         repaint();
     });
     document.addEventListener('keypress', function (event) {
+        keylabel.innerText = event.code;
         if (poly.state != PolygonState.POLYGON_DONE) {
             return;
         }
@@ -570,6 +585,12 @@
                 points.length = 0;
                 clipPoly.state = PolygonState.POLYGON_OUTER;
                 console.log('clipping');
+                break;
+            case 105:// i
+                var r = parseInt(prompt('new color (r):', '0'));
+                var g = parseInt(prompt('new color (g):', '0'));
+                var b = parseInt(prompt('new color (b):', '0'));
+                color = new Color(r, g, b);
                 break;
         }
         repaint();
