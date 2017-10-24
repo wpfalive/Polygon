28080e85ed021c7a668be5bcf257e858df067b9d
TennnyZhuang <zty0826@gmail.com>
2017-10-24 16:02:37 +0800

    Implement polygon clip with Weiler-Atherton algorithm

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,10 @@
   const ctx = canvas.getContext('2d');
   ctx.fillStyle = 'black';
 
-  const points: Point[] = [];
+  let points: Point[] = [];
 
   enum PolygonState {
+    POLYGON_INIT,
     POLYGON_OUTER,
     POLYGON_INNER,
     POLYGON_DONE,
@@ -42,13 +43,83 @@
       this._y = Math.floor(v);
     }
 
-    nearBy(p2: Point): boolean {
-      return Math.abs(p2.x - this.x) < 3 && Math.abs(p2.y - this.y) < 3;
+    dis2(p2: Point): number {
+      return (p2.x - this.x) * (p2.x - this.x) +
+          (p2.y - this.y) * (p2.y - this.y);
     }
 
-    draw() {
-      ctx.fillRect(this.x, this.y, 3, 3);
+    dis(p2: Point): number {
+      return Math.sqrt(this.dis2(p2));
     }
+
+    on(p1: Point, p2: Point): boolean {
+      return (p1.x - this.x) * (p2.y - this.y) ===
+          (p1.y - this.y) * (p2.x - this.x);
+    }
+
+    in(poly2: Polygon): boolean {
+      const n = poly2.outerPoints.length;
+      let angle = 0;
+      for (let i = 0; i < n; ++i) {
+        const p1 = poly2.outerPoints[i];
+        const p2 = poly2.outerPoints[(i + 1) % n];
+        const a2 = p1.dis2(p2);
+        const b2 = this.dis2(p1);
+        const c2 = this.dis2(p2);
+        const b = Math.sqrt(b2);
+        const c = Math.sqrt(c2);
+        const cosv = (b2 + c2 - a2) / (2 * b * c);
+        const v = Math.acos(cosv);
+        angle += (cross(this, p1, p2) > 0) ? v : -v;
+      }
+
+      return Math.abs(angle) > 1e-6;
+    }
+
+    equal(p2: Point) {
+      return this.x === p2.x && this.y === p2.y;
+    }
+  }
+
+  function cross(p1: Point, p2: Point, p3: Point): number {
+    return (p2.x - p1.x) * (p3.y - p1.y) - (p3.x - p1.x) * (p2.y - p1.y);
+  }
+
+  function cross2(p1: Point, p2: Point, p3: Point, p4: Point): number {
+    return (p2.x - p1.x) * (p4.y - p3.y) - (p2.y - p1.y) * (p4.x - p3.x);
+  }
+
+  function intersect(p1: Point, p2: Point, p3: Point, p4: Point): Point|null {
+    const cross1 = cross(p3, p4, p1);
+    const cross2 = cross(p3, p4, p2);
+    const cross3 = cross(p1, p2, p3);
+    const cross4 = cross(p1, p2, p4);
+
+    let p: Point;
+    if (((cross1 > 0 && cross2 < 0) || (cross1 < 0 && cross2 > 0)) &&
+        ((cross3 < 0 && cross4 > 0) || (cross3 > 0 && cross4 < 0))) {
+      const nx = (p3.x * cross4 - p4.x * cross3) / (cross4 - cross3);
+      const ny = (p3.y * cross4 - p4.y * cross3) / (cross4 - cross3);
+      p = new Point(nx, ny);
+      return p;
+    } else if (p1.on(p3, p4)) {
+      p = p1;
+      return null;
+    }
+    if (p2.on(p3, p4)) {
+      p = p2;
+      return null;
+    }
+    if (p3.on(p1, p2)) {
+      p = p3;
+      return null;
+    }
+    if (p4.on(p1, p2)) {
+      p = p4;
+      return null;
+    }
+
+    return null;
   }
 
   class Polygon {
@@ -63,6 +134,25 @@
       });
       const n = this.outerPoints.length;
       return new Point(psum.x / n, psum.y / n);
+    }
+
+    get segments(): [Point, Point][] {
+      const s: [Point, Point][] = [];
+      for (let i = 0; i < this.outerPoints.length; ++i) {
+        s.push([
+          this.outerPoints[i],
+          this.outerPoints[(i + 1) % this.outerPoints.length],
+        ]);
+      }
+
+      for (let i = 0; i < this.innerPoints.length; ++i) {
+        s.push([
+          this.innerPoints[i],
+          this.innerPoints[(i + 1) % this.innerPoints.length],
+        ]);
+      }
+
+      return s;
     }
 
     constructor(outerPoints: Point[] = [], innerPoints: Point[] = []) {
@@ -216,9 +306,120 @@
         p.x = 2 * c.x - p.x;
       }
     }
+
+    intersect(p1: Point, p2: Point, from: Point): [Point, number] {
+      let min = Infinity;
+      let ans: [Point, number] = [null, -1];
+      for (let i = 0; i < this.outerPoints.length; ++i) {
+        const p3 = this.outerPoints[i];
+        const p4 = this.outerPoints[(i + 1) % this.outerPoints.length];
+
+        const p = intersect(p1, p2, p3, p4);
+        if (p) {
+          if (p1.dis2(p) > p1.dis2(from) && p1.dis2(p) < min) {
+            min = p1.dis2(p);
+            ans = [p, i];
+          }
+        }
+      }
+
+      return ans;
+    }
+
+    clip(poly2: Polygon): any {
+      let st1: number;
+      let st2: number;
+      let flag: boolean;
+      let p: Point = null;
+      out: for (let i = 0; i < this.outerPoints.length; ++i) {
+        for (let j = 0; j < poly2.outerPoints.length; ++j) {
+          const p1 = this.outerPoints[i];
+          const p2 = this.outerPoints[(i + 1) % this.outerPoints.length];
+          const p3 = poly2.outerPoints[j];
+          const p4 = poly2.outerPoints[(j + 1) % poly2.outerPoints.length];
+          p = intersect(p1, p2, p3, p4);
+          if (p) {
+            st1 = i;
+            st2 = j;
+            flag = cross2(p1, p2, p3, p4) > 0;
+            break out;
+          }
+        }
+      }
+
+      if (p === null) {
+        console.log('no intersects found');
+        if (this.outerPoints[0].in(poly2)) {
+          return this.outerPoints.slice();
+        } else if (poly2.outerPoints[0].in(this)) {
+          return poly2.outerPoints.slice();
+        } else {
+          return [];
+        }
+      }
+
+      let i = st1;
+      let j = st2;
+      console.log(i, j, flag);
+
+      const intersections: Point[] = [p];
+      do {
+        if (flag) {
+          let idx;
+          do {
+            console.log(i, j);
+            const p3 = poly2.outerPoints[j];
+            const p4 = poly2.outerPoints[(j + 1) % poly2.outerPoints.length];
+            [p, idx] = this.intersect(p3, p4, p);
+            if (idx === -1) {
+              j = (j + 1) % poly2.outerPoints.length;
+              p = p4;
+            }
+          } while (idx === -1);
+
+          while (i !== idx) {
+            console.log(i, j);
+
+            i = (i + 1) % this.outerPoints.length;
+            intersections.push(this.outerPoints[i]);
+          }
+          intersections.push(p);
+          flag = !flag;
+        } else {
+          let idx;
+          do {
+            console.log(i, j);
+            const p1 = this.outerPoints[i];
+            const p2 = this.outerPoints[(i + 1) % this.outerPoints.length];
+            [p, idx] = poly2.intersect(p1, p2, p);
+            if (idx === -1) {
+              i = (i + 1) % this.outerPoints.length;
+              p = p2;
+            }
+          } while (idx === -1);
+
+          while (j !== idx) {
+            console.log(i, j);
+
+            j = (j + 1) % poly2.outerPoints.length;
+            intersections.push(poly2.outerPoints[j]);
+          }
+          intersections.push(p);
+          flag = !flag;
+        }
+      } while (i !== st1 || j !== st2);
+
+      if (intersections[intersections.length - 1].equal(intersections[0])) {
+        intersections.pop();
+      }
+
+      return intersections;
+    }
   }
 
   let poly = new Polygon();
+  let clipPoly = new Polygon();
+  clipPoly.state = PolygonState.POLYGON_INIT;
 
   let focus: Point|null = null;
 
@@ -255,7 +456,10 @@
 
     ctx.clearRect(0, 0, width, height);
 
-    if (poly.state !== PolygonState.POLYGON_DONE && points.length >= 1) {
+    if (((poly.state !== PolygonState.POLYGON_DONE) ||
+         (clipPoly.state !== PolygonState.POLYGON_INIT) ||
+         (clipPoly.state !== <PolygonState>PolygonState.POLYGON_DONE)) &&
+        points.length >= 1) {
       ctx.beginPath();
       const n = points.length;
       for (let i = 0; i < n - 1; ++i) {
@@ -273,6 +477,10 @@
         poly.fill();
       }
     }
+
+    if (clipPoly) {
+      clipPoly.drawBorder();
+    }
   }
 
   canvas.addEventListener('mousemove', (event) => {
@@ -281,15 +489,27 @@
   });
 
   canvas.addEventListener('contextmenu', (event) => {
+    console.log(poly);
     if (poly.state === PolygonState.POLYGON_OUTER) {
       poly.state = PolygonState.POLYGON_INNER;
       poly.outerPoints = points.slice();
     } else if (poly.state === PolygonState.POLYGON_INNER) {
       poly.state = PolygonState.POLYGON_DONE;
       poly.innerPoints = points.slice();
-    } else {
-      return;
+    } else if (clipPoly.state === PolygonState.POLYGON_OUTER) {
+      // TODO: inner hole of clip polygon
+      console.log(poly, clipPoly);
+      clipPoly.state = PolygonState.POLYGON_DONE;
+      clipPoly.outerPoints = points.slice();
+      setTimeout(() => {
+        poly.outerPoints = poly.clip(clipPoly);
+        poly.innerPoints = [];
+        clipPoly.state = PolygonState.POLYGON_INIT;
+        clipPoly.outerPoints = [];
+        repaint();
+      }, 30);
     }
+
     points.length = 0;
     event.preventDefault();
     repaint();
@@ -306,25 +526,32 @@
     }
 
     switch (event.charCode) {
-      case 109: // m
-      const dx = parseInt(prompt('move x:', '0'));
-      const dy = parseInt(prompt('move y:', '0'));
-      poly.move(dx, dy);
-      break;
+      case 109:  // m
+        const dx = parseInt(prompt('move x:', '0'));
+        const dy = parseInt(prompt('move y:', '0'));
+        poly.move(dx, dy);
+        break;
 
-      case 114: // r
-      const angle = Number(prompt('rotate r: (in degree)'));
-      poly.rotate(angle);
-      break;
+      case 114:  // r
+        const angle = Number(prompt('rotate r: (in degree)'));
+        poly.rotate(angle);
+        break;
 
-      case 115: // s
-      const k = Number(prompt('scale k: '));
-      poly.scale(k);
-      break;
+      case 115:  // s
+        const k = Number(prompt('scale k: '));
+        poly.scale(k);
+        break;
 
-      case 102: // f
-      const dir = prompt('flip in which axis (x or y)') === 'x';
-      poly[dir ? 'flipX' : 'flipY']();
+      case 102:  // f
+        const dir = prompt('flip in which axis (x or y)') === 'x';
+        poly[dir ? 'flipX' : 'flipY']();
+        break;
+
+      case 99:  // c
+        points.length = 0;
+        clipPoly.state = PolygonState.POLYGON_OUTER;
+        console.log('clipping');
+        break;
     }
 
     repaint();
